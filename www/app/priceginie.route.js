3b09871930fc64694b24713a47ac80bc099a6036
deepak <ujjawal@excellencetechnologies.in>
2015-12-21 14:22:15 +0530

    Structure modification

--- www/app/priceginie.route.js
+++ www/app/priceginie.route.js
@@ -1,64 +1,7 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-var myapp = angular.module('starter', ['ionic', 'startercc', 'angular-chartist']);
-
-
-myapp.run(function($ionicPlatform, urlHelper, $rootScope) {
-    document.addEventListener("offline", onOffline, false);
-    function onOffline() {
-        console.log('hello1');
-        //   window.plugins.toast.showShortBottom('You Are Offline Now');
-        urlHelper.openOffline();
-    }
-    document.addEventListener("online", onOnline, false);
-    function onOnline() {
-        urlHelper.openHome();
-        console.log("hello2");
-        //  window.plugins.toast.showShortBottom('You Are Online Now');
-    }
-
-    $ionicPlatform.ready(function() {
-        var isWebView = ionic.Platform.isWebView();
-        console.log(isWebView);
-        try {
-            if (device.platform == 'Android') {
-                urlHelper.openHome();
-                StatusBar.backgroundColorByHexString("#06457b");
-            }
-        }
-        catch (e) {
-
-
-        }
-        if (isWebView) {
-            urlHelper.openHome();
-        }
-
-    });
-// this is for front view of app 
-//        if (!timeStorage.get("login") && !timeStorage.get("googleLogin") && !timeStorage.get("fbLogin")) {
-//
-//            $rootScope.show = true;
-//            $rootScope.show1 = false;
-//            // urlHelper.openFrontpage();
-//            urlHelper.openHome();
-//        }
-//        else {
-//            urlHelper.openHome();
-//        }
-
-
-    // urlHelper.openHome();
-    //alert(1);
-
-
-
-
-});
-myapp.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+(function() {
+    'use strict';
+    angular.module('starter')
+    .config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
 // if (ionic.Platform.isAndroid()) {
 //      $ionicConfigProvider.scrolling.jsScrolling(false);
 //    }
@@ -72,7 +15,7 @@ myapp.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) 
 //    })
             .state('menu', {
                 url: '/priceGenie',
-                templateUrl: 'partials/menu.html',
+                templateUrl: 'app/home/menu.html',
                 controller: 'menuCtrl',
                 // abstract: true
             })
@@ -80,126 +23,126 @@ myapp.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) 
             .state('menu.home', {
                 url: '/home',
                 views: {'mainView': {
-                        templateUrl: 'partials/home.html',
+                        templateUrl: 'app/home/home.html',
                         controller: 'homeCtrl'
                     }}
             })
             .state('menu.category', {
                 url: '/category/:name/:category/:subCategory/:brand',
                 views: {'mainView': {
-                        templateUrl: 'partials/viewall.html',
+                        templateUrl: 'app/product/viewall.html',
                         controller: 'viewallCtrl'
                     }}
             })
             .state('menu.search', {
                 url: '/search',
                 views: {'mainView': {
-                        templateUrl: 'partials/search.html',
+                        templateUrl: 'app/product/search.html',
                         controller: 'searchCtrl'
                     }}
             })
             .state('menu.product', {
                 url: '/product/:name/:query_id/',
                 views: {'mainView': {
-                        templateUrl: 'partials/product.html',
+                        templateUrl: 'app/product/product.html',
                         controller: 'productCtrl'
                     }}
             })
             .state('menu.register', {
                 url: '/register',
                 views: {'mainView': {
-                        templateUrl: 'partials/register.html',
+                        templateUrl: 'app/register/register.html',
                         controller: 'registerCtrl'
                     }}
             })
             .state('menu.login', {
                 url: '/login',
                 views: {'mainView': {
-                        templateUrl: 'partials/login.html',
+                        templateUrl: 'app/login/login.html',
                         controller: 'loginCtrl'
                     }}
             })
             .state('menu.setting', {
                 url: '/setting',
                 views: {'mainView': {
-                        templateUrl: 'partials/setting.html',
+                        templateUrl: 'app/setting/setting.html',
                         controller: 'settingCtrl'
                     }}
             })
             .state('menu.feedback', {
                 url: '/feedback',
                 views: {'mainView': {
-                        templateUrl: 'partials/feedback.html',
+                        templateUrl: 'app/feedback/feedback.html',
                         controller: 'feedbackCtrl'
                     }}
             })
             .state('menu.recentSearches', {
                 url: '/recent',
                 views: {'mainView': {
-                        templateUrl: 'partials/myRecentSearches.html',
+                        templateUrl: 'app/product/myRecentSearches.html',
                         controller: 'recentSearches'
                     }}
             })
             .state('menu.Aboutus', {
                 url: '/Aboutus',
                 views: {'mainView': {
-                        templateUrl: 'partials/Aboutus.html',
+                        templateUrl: 'app/about/Aboutus.html',
                         controller: 'AboutusCtrl'
                     }}
             })
             .state('menu.star', {
                 url: '/notification',
                 views: {'mainView': {
-                        templateUrl: 'partials/star.html',
+                        templateUrl: 'app/common/star.html',
                         controller: 'star'
                     }}
             })
             .state('menu.notification', {
                 url: '/category',
                 views: {'mainView': {
-                        templateUrl: 'partials/notification.html',
+                        templateUrl: 'app/common/notification.html',
                         controller: 'star'
                     }}
             })
             .state('menu.Contactus', {
                 url: '/Contactus',
                 views: {'mainView': {
-                        templateUrl: 'partials/Contactus.html',
+                        templateUrl: 'app/contact/Contactus.html',
                         controller: 'ContactusCtrl'
                     }}
             })
             .state('menu.YourAlert', {
                 url: '/YourAlert',
                 views: {'mainView': {
-                        templateUrl: 'partials/YourAlert.html',
+                        templateUrl: 'app/common/YourAlert.html',
                         controller: 'YourAlertCtrl'
                     }}
             })
             .state('menu.website', {
                 url: '/website',
                 views: {'mainView': {
-                        templateUrl: 'partials/website.html',
+                        templateUrl: 'app/common/website.html',
                         controller: 'star'
                     }}
             })
             .state('menu.notifytext', {
                 url: '/text',
                 views: {'mainView': {
-                        templateUrl: 'partials/notifytext.html',
+                        templateUrl: 'app/common/notifytext.html',
                         controller: 'star'
                     }}
             })
             .state('menu.success', {
                 url: '/successful',
                 views: {'mainView': {
-                        templateUrl: 'partials/notifySuccess.html',
+                        templateUrl: 'app/common/notifySuccess.html',
                         controller: 'star'
                     }}
             })
             .state('menu.offline', {
                 url: '/offline',
                 views: {'mainView': {
-                        templateUrl: 'partials/offline.html',
+                        templateUrl: 'app/common/offline.html',
                         controller: 'recentSearches'
                     }}
             });
@@ -208,23 +151,4 @@ myapp.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) 
     // $urlRouterProvider.otherwise('/priceGenie/home');
 
 });
-
-myapp.directive('menuCloseKeepHistory', ['$ionicHistory', function($ionicHistory) {
-        return {
-            restrict: 'AC',
-            link: function($scope, $element) {
-                $element.bind('click', function() {
-                    var sideMenuCtrl = $element.inheritedData('$ionSideMenusController');
-                    if (sideMenuCtrl) {
-                        $ionicHistory.nextViewOptions({
-                            historyRoot: false,
-                            disableAnimate: true,
-                            expire: 300
-                        });
-                        sideMenuCtrl.close();
-                    }
-                });
-            }
-        };
-    }]);
-var myapps = angular.module('starterss', ['ionic', 'startercc', 'ngStorage']);
+})();
